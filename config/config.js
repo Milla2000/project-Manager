caa687bd607b15f79dbf8880e12b479c0eac1701
Milla Jesso <89450003+Milla2000@users.noreply.github.com>
2023-08-25 08:52:47 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const sqlConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
-    server: 'localhost', // or '127.0.0.1'
+    server: process.env.DB_SERVER, // or '127.0.0.1'
     pool: {
         max: 10,
         min: 0,
